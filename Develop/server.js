2ac4677650469c44b34a2226ea78243bd464d182
Zach <z.d.jorgensen@gmail.com>
2022-08-28 03:55:53 -0600

    created sever.js

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+// const api = require('./public/assets/js/index.js');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT}`)
+);
